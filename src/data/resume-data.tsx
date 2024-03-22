2c7655cd4efa8844d9e7b5a2f05d230c5ae82a07
dnovac <danynovac@gmail.com>
2024-03-22 14:15:08 +0200

    updates with my data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,135 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Dan Novac",
+  initials: "DN",
+  location: `Brasov, Romania, EET`,
+  locationLink: "https://www.google.com/maps/place/Brasov",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Senior Software Engineer | Backend | Cloud ",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Software Backend Engineer, I have successfully converted ideas into features and products. I analyse, prioritize and implement concepts & architectural decisions in order to enhance the product and I trust the process when everyone from a team understands the product and the details. Currently, I work mostly with Node.js, Typescript and AWS Cloud Services. I have over 9 years of experience as a software developer and I work remotely.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/8904832?v=4",
+  personalWebsiteUrl: "https://www.dan-novac.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "danynovac@gmail.com",
+    // tel: "+0729530202 - not me, don't callme",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/dnovac",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/dan-novac-40062974/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/daninovac",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Transilvania University Brasov: Computer Science & Mathematics",
+      degree: "Bachelor's Degree in Computer Science",
+      start: "2012",
+      end: "2015",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Ateliere",
+      link: "https://www.ateliere.com/",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
+      title: "Senior Software Engineer",
       start: "2021",
-      end: "2024",
+      end: "present",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Migrated services to Serverless infrastructure. Developed quality and performant solutions using AWS Cloud infrastructure and services, such as: Lambda, DynamoDB, CloudFormation, Step Functions, SQS, SNS, Cloudwatch. Implemented new features using Typescript, Node.js, GraphQL, Terraform, Bash scripts. Worked with media industry tools such as ffmpeg, dolby. Developed infra orchestrating solutions using Kubernetes, Docker and Keda.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
+      company: "Pentalog",
+      link: "https://www.pentalog.com/",
+      badges: ["On-site", "Remote"],
+      title: "FullStack Software Developer, Senior Software Engineer",
+      start: "2014",
       end: "2021",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Implemented new features. Developed high-quality software solutions using Java, Kotlin, Spring, Docker, Node.js, Kubernetes, Javascript. Created from scratch new microservices and infrastructure. Enhanced performance for big data on different projects and databases. Developed end-to-end API. Developed & Orchestrated solutions in order to accommodate data from third parties services. Enhanced and migrated databases from SQL to NoSQL. Digitalized factories using data from PLCs.",
     },
   ],
   skills: [
-    "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Serverless",
+    "AWS Cloud Services",
+    "Java",
+    "Kubernetes",
+    "Docker",
+    "Terraform",
+    "SQS",
+    "SNS",
+    "DynamoDB",
+    "Cloudwatch",
+    "System Parameter Store",
+    "Step Functions",
+    "Javascript",
+    "Postgres",
+    "MongoDB",
+    "SQL",
+    "CSS",
+    "Spring Framework",
+    "Webpack",
+    "Next.js",
+    "Autonomy",
+    "Clean Code",
+    "Cost reduction",
+    "Analytical thinking",
+    "Mentoring & Coaching",
+    "Motivator",
+    "Consistency"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Simple Tasks",
+      techStack: ["Side Project", "Javascript", "React.js"],
+      description:
+        "The app simple-tasks can be used to track routine/daily tasks that does not require much than a couple of minutes to complete.",
+      link: {
+        label: "simple-tasks",
+        href: "https://dnovac.github.io/simple-tasks/",
+      },
+    },
+    {
+      title: "Batch Jobs",
       techStack: [
         "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
+        "Java",
+        "Docker",
+        "Spring Boot",
+        "Spring Batch",
         "WebRTC",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "This piece of software, developed using Java, Spring Boot and Spring Batch, exposes an API which handles import of CSV files and XML files into a NoSQL MongoDB database.",
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "batch-jobs",
+        href: "https://github.com/dnovac/batch-jobs",
       },
     },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
 
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
+    {
+      title: "Shelves",
+      techStack: ["Side Project", "Typescript", "Docker", "Express", "TypeORM"],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+        "Personal backend project used to track all my personal collections, such as books, vinyls, games etc.",
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "shelves",
+        href: "https://github.com/dnovac/shelves",
       },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
